b07b154a41977907e1e30dacb4cd93061f7dbe37
Aaron Castanos <AGCastanos@taylorscitechphil.com>
2017-10-25 16:59:20 +0800

    Add project files.

---
+++ ServiceTopshelf/Program.cs
@@ -0,0 +1,17 @@
+﻿using ServiceTopshelf.Topshelf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTopshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureService.Configure();
+        }
+    }
+}
---
+++ ServiceTopshelf/Topshelf/ConfigureService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ServiceTopshelf.Topshelf
+{
+    internal static class ConfigureService
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<VendorApiService>(service =>
+                {
+                    service.ConstructUsing(s => new VendorApiService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("VendorApiService");
+                configure.SetDisplayName("VP VendorApi Service");
+                configure.SetDescription("Vendor Portal VendorApi Service (Curtis 1000)");
+            });
+        }
+    }
+}
